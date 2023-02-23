eb264210910405c8d6c8740194241df5382be353
Francesca Santoriello <f.santoriello@hotmail.com>
2023-02-23 11:52:58 +0100

    lecture + vrb

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+
+  }
+
+  updateNavbar() {
+    if (window.scrollY >= window.innerHeight) {
+      this.element.classList.add("navbar-lewagon-white")
+      console.log('scroll ok')
+    } else {
+      this.element.classList.remove("navbar-lewagon-white")
+    }
+  }
+}
